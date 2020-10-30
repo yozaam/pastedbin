3415d6555d180139b0f474bff2c58464bfb6bfe3
yozaam <yozaam@gmail.com>
2020-10-30 18:04:14 +0530

    making the static files folder

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.get('/', function(req,res){
+	if (!fs.existsSync('./static')){
+	    fs.mkdirSync('./static');
+	}
+	if (!fs.existsSync('prev_id.txt')){
+	    fs.writeFileSync('prev_id.txt','0',function(err){
+			if (err) return console.error(err);
+		});
+	}
 	res.sendFile(path.join(__dirname + '/index.html'));
 });
 
@@ -21,11 +29,12 @@ app.post('/', function(req, res){
    fs.readFile('prev_id.txt',function(err,data){
    	if (err) return console.error(err);
    	var id = data.toString();
-	console.log('writing ',paste,' to ',id);
+	console.log('writing ',paste,' to ',id+'.txt');
 	fs.writeFile('./static/'+id+'.txt',paste , function(err) {
 		if (err) return console.error(err);
 	});
-	res.end('your file is at lhttps://pastedbin.herokuapp.com/'+id);
+	res.setHeader('Content-type','text/html')
+	res.end("<h1>your file is at <a href='https://pastedbin.herokuapp.com/'>https://pastedbin.herokuapp.com/"+id+'</a></h1>');
 	// now update the id
 	var next_id = parseInt(id) + 1 + '';
 	console.log('writing ',next_id,'to the prev_id.txt');
